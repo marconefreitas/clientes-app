2d14783bcb57273c416d4b7f27c4c6e4470b9839
Marcone Freitas <marcfreitas0@gmail.com>
2020-12-22 19:14:53 -0300

    Tela de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,20 +11,24 @@ import {ClientesService} from './clientes.service';
 import {HttpClientModule} from '@angular/common/http';
 import {ServicoPrestadoModule} from './servico-prestado/servico-prestado.module';
 import { ServicoPrestadoService } from './servico-prestado.service';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
 
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule,
     AppRoutingModule,
     TemplateModule,
     ClientesModule,
     ServicoPrestadoModule
+    
   ],
   providers: [ClientesService, ServicoPrestadoService],
   bootstrap: [AppComponent]
